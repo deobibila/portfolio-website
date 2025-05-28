dfdb121949536a4426ce9dca10e5e1a63f732351
deogratias99 <40279863+deogratias99@users.noreply.github.com>
2025-05-28 12:11:08 -0400

    Fixed Hovering Title for /posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -4,12 +4,17 @@ import Container from "../../components/container";
 import distanceToNow from "../../lib/dateRelative";
 import { getAllPosts } from "../../lib/getPost";
 import { generateRSSFeed} from "../../scripts/generate-rss";
+import Head from "next/head";
 
 export default function NotePage({
   allPosts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Container>
+      <Head>
+        <title>My Blog Posts</title>
+        <meta name="description" content="Blog posts by Deo" />
+      </Head>
       {allPosts.length ? (
         allPosts.map((post) => (
           <article key={post.slug} className="mb-10">
