834b7e97b195ffa688b4fcaf4c1fdaa2bc6b64c6
deogratias99 <40279863+deogratias99@users.noreply.github.com>
2025-06-01 19:29:14 -0400

    Added H1 to posts/index.tsx

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -10,53 +10,56 @@ export default function NotePage({
   allPosts,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <Container>
-      <Head>
-        <title>Deo's Blog</title>
-        <meta name="description" content="Blog posts by Deo"/>
-        <meta name="author" content="Deo Bibila"/>
+      <Container>
+        <Head>
+          <title>Deo's Blog</title>
+          <meta name="description" content="Blog posts by Deo"/>
+          <meta name="author" content="Deo Bibila"/>
 
-        <meta property="og:title" content="Deo's Blog"/>
-        <meta property="og:description" content="Blog posts by Deo Bibila on tech & research."/>
-        <meta property="og:type" content="website"/>
-        <meta property="og:url" content="https://www.deobibila.com/posts"/>
-        <meta property="og:image" content="https://www.deobibila.com/images/default-og.jpg"/>
-        <meta property="og:image:alt" content="A preview of Deo's blog homepage"/>
-        <meta property="og:image:width" content="1200"/>
-        <meta property="og:image:height" content="630"/>
+          <meta property="og:title" content="Deo's Blog"/>
+          <meta property="og:description" content="Blog posts by Deo Bibila on tech & research."/>
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content="https://www.deobibila.com/posts"/>
+          <meta property="og:image" content="https://www.deobibila.com/images/default-og.jpg"/>
+          <meta property="og:image:alt" content="A preview of Deo's blog homepage"/>
+          <meta property="og:image:width" content="1200"/>
+          <meta property="og:image:height" content="630"/>
 
-        {/* Twitter Tags */}
-        <meta name="twitter:card" content="summary_large_image"/>
-        <meta name="twitter:title" content="Deo's Blog"/>
-        <meta name="twitter:description" content="Blog posts by Deo Bibila on tech & research."/>
-        <meta name="twitter:image" content="https://www.deobibila.com/images/default-og.jpg"/>
+          {/* Twitter Tags */}
+          <meta name="twitter:card" content="summary_large_image"/>
+          <meta name="twitter:title" content="Deo's Blog"/>
+          <meta name="twitter:description" content="Blog posts by Deo Bibila on tech & research."/>
+          <meta name="twitter:image" content="https://www.deobibila.com/images/default-og.jpg"/>
 
-        <link rel="canonical" href="https://www.deobibila.com/posts"/>
-      </Head>
-      {allPosts.length ? (
-          allPosts.map((post) => (
-              <article key={post.slug} className="mb-10">
-                <Link
-                    as={`/posts/${post.slug}`}
-                    href="/posts/[slug]"
-                    className="text-lg leading-6 font-bold"
-                >
-                  {post.title}
-                </Link>
-                <p>{post.excerpt}</p>
-                <div className="text-gray-400">
-                  <time>
-                    {new Date(post.date).toLocaleString('en-US', {
-                      dateStyle: 'long'
-                    })}
-                  </time>
-                </div>
-              </article>
-          ))
-      ) : (
-          <p>No blog posted yet :/</p>
-      )}
-    </Container>
+          <link rel="canonical" href="https://www.deobibila.com/posts"/>
+        </Head>
+
+        <h1 className="text-2xl font-bold mb-6">All Blog Posts</h1>
+
+        {allPosts.length ? (
+            allPosts.map((post) => (
+                <article key={post.slug} className="mb-10">
+                  <Link
+                      as={`/posts/${post.slug}`}
+                      href="/posts/[slug]"
+                      className="text-lg leading-6 font-bold"
+                  >
+                    {post.title}
+                  </Link>
+                  <p>{post.excerpt}</p>
+                  <div className="text-gray-400">
+                    <time>
+                      {new Date(post.date).toLocaleString('en-US', {
+                        dateStyle: 'long'
+                      })}
+                    </time>
+                  </div>
+                </article>
+            ))
+        ) : (
+            <p>No blog posted yet :/</p>
+        )}
+      </Container>
   );
 }
 
